48288a75f875c56dac80fb675abfc87416c30ee9
fukuiretu <fukuiretu@gmail.com>
2022-01-02 21:32:07 +0900

    refactor

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,5 +1,16 @@
 use std::fs::read_to_string;
 
+struct GrapeArgs {
+    path: String,
+    pattern: String,
+}
+
+impl GrapeArgs {
+    fn new(pattern: String, path: String) -> GrapeArgs {
+        GrapeArgs { path, pattern }
+    }
+}
+
 fn grep(content: String, pattern: String) {
     for line in content.lines() {
         if line.contains(pattern.as_str()) {
@@ -8,9 +19,9 @@ fn grep(content: String, pattern: String) {
     }
 }
 
-fn run(path: String, pattern: String) {
-    match read_to_string(path) {
-        Ok(content) => grep(content, pattern),
+fn run(state: GrapeArgs) {
+    match read_to_string(state.path) {
+        Ok(content) => grep(content, state.pattern),
         Err(reason) => println!("{}", reason),
     }
 }
@@ -20,7 +31,7 @@ fn main() {
     let path = std::env::args().nth(2);
 
     match (pattern, path) {
-        (Some(pattern), Some(path)) => run(path, pattern),
+        (Some(pattern), Some(path)) => run(GrapeArgs::new(pattern, path)),
         _ => println!("pattern or path is not specified!"),
     }
 }
